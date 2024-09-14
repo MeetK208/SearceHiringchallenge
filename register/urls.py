ac0ceb371624f305f4bfe84ecc1378aaed7e65fb
meetk <meetkothari208@gmail.com>
2024-09-14 23:03:19 +0530

    folder structure changes

--- register/urls.py
+++ register/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('auth/', include('register.urls')),
-    path('user-projects/', include('projectcard.urls')),
-    path('project-list/', include('usercard.urls')),
+    path('getuser', views.getData),
+    path('register', views.postData),
+    path('login/', views.loginData,name='login'),
 ]
